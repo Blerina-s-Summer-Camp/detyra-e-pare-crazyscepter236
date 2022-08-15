60cb0d76a4a9c1aaea14453f4924d75e5dfad88d
edadobra <110973861+crazyscepter236@users.noreply.github.com>
2022-08-15 02:34:11 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+spiral = turtle.Turtle()
+
+    print("    /\\    ")
+    print("   /  \\   ")
+    print("   - - -   ")
+    print("  |     |  ")
+    print("  | |-| |  ")
+    print("  |     |  ")
+
+turtle.done()
